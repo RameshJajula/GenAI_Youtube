5b30b2133f71a2580bc6140f7e491657c53101ca
RameshJ <RRSHfamily@gmail.com>
2024-01-19 13:52:29 +0530

    updated

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ from datetime import datetime, timedelta
 import googleapiclient.discovery
 import googleapiclient.errors
 import PIL
+import google.generativeai as genai
+
+# Import Markdown display for rendering generated content
+from IPython.display import Markdown
+
 # Path to your banner image (can be a local path or a URL)
 banner_image = PIL.Image.open("C:\\Users\\rames\\OneDrive\\Desktop\\Logo\\Website.jpg")  # Replace with your image path
 
 # Display the banner image
 st.image(banner_image,width=None, use_column_width='always')
+
+
 
 def get_videos(api_key, term, date_string):
     try:
@@ -81,10 +88,12 @@ def get_transcript(video_url):
         return None
 
     try:
+        
         loader = YoutubeLoader.from_youtube_url(video_url, add_video_info=True)
+        
         return loader.load()
     except Exception as e:
-        print(f"An error occurred while fetching the transcript: {e}")
+        st.write(f"An error occurred while fetching the transcript: {e}")
         return None
         
 
@@ -97,52 +106,112 @@ def fetch_videos(api_key, search_term, date_string):
 
 def fetch_transcript(url):
     try:
+        
         return get_transcript(url)
+        
     except Exception as e:
         st.error(f"Error fetching transcript: {e}")
         return None
+import textwrap
+
+def split_text_to_multiline(text, max_line_length):
+    return '\n'.join(textwrap.wrap(text, max_line_length))
 
 def display_top_videos(videos):
     df = pd.DataFrame(videos)
-    df = df.sort_values('view_count', ascending=False).head(3)  # Top 3 videos
+    st.write(df)
+    #df = df.sort_values('view_count', ascending=False)  # Top 3 videos
+    #st.write(df)
+    df1 = df.sort_values('view_count', ascending=False).head(3)
     st.subheader('Top 3 Videos')
-    st.table(df[['title', 'url', 'view_count']])
-    return df
+    st.table(df1[['title', 'url', 'view_count']])
+    return df1
 
 def main():
     st.title('QuickVid Summarizer: YouTube Content at a Glance')
-    st.markdown("## Please input your API keys")
+    st.sidebar.markdown("## Please input your API keys")
     date_string = (datetime.now() - timedelta(days=5)).isoformat("T") + "Z"
+    # Create a list of activity options for selection
+    activity = ['OpenAI', 'GeminiPro']
 
-    youtube_api_key = st.text_input("Insert your YouTube API key here", type="password")
-    openai_api_key = st.text_input("Insert your OpenAI API key here", type="password")
+    # Use Streamlit's sidebar to allow users to select the model type
+    choice = st.sidebar.selectbox("Select Model type", activity)
 
-    if st.button('Submit'):
+    youtube_api_key = st.sidebar.text_input("Insert your YouTube API key here", type="password")
+    openai_api_key = st.sidebar.text_input("Insert your "+choice+"  API key here", type="password")
+
+
+
+
+    if st.sidebar.button('Submit'):
         st.session_state['api_keys'] = {'youtube': youtube_api_key, 'openai': openai_api_key}
 
     if 'api_keys' in st.session_state:
-        search_term = st.text_input('Enter search term', 'AI news')
+        search_term = st.text_input('Enter a keyword or phrase to search for YouTube videos:', 'LLM news')
 
         if st.button('Search'):
             with st.spinner('Fetching Videos...'):
                 st.session_state['videos'] = fetch_videos(st.session_state['api_keys']['youtube'], search_term, date_string)
-
+                
         if st.session_state.get('videos'):
             df = display_top_videos(st.session_state['videos'])
             selected_video_url = st.selectbox("Select a video for transcript:", df['url'])
 
             if st.button('Get Transcript'):
+                
                 with st.spinner('Fetching Transcript...'):
+                    
                     transcript = fetch_transcript(selected_video_url)
+                    docs = list(transcript)
+                    # Initialize an empty list to store extracted data
+                    data = []
+
+                    # Iterate over the list and extract data
+                    for doc in docs:
+                        data.append({
+                            "Page Content": doc.page_content,
+                            "Source": doc.metadata['source'],
+                            "Title": doc.metadata['title'],
+                            "Description": doc.metadata['description'],
+                            "View Count": doc.metadata['view_count'],
+                            "Thumbnail URL": doc.metadata['thumbnail_url'],
+                            "Publish Date": doc.metadata['publish_date'],
+                            "Length": doc.metadata['length'],
+                            "Author": doc.metadata['author']
+                        })
+                    
+                    # Creating DataFrame
+                    
+                    df = pd.DataFrame(data)
+ 
+                    # Display DataFrame in Streamlit
+                    st.write(df)
+                    st.write(transcript)
                     if transcript:
+                        
                         st.session_state['transcript'] = transcript
 
         if st.session_state.get('transcript'):
-            llm = OpenAI(openai_api_key=st.session_state['api_keys']['openai'], temperature=0.5)
-            chain = load_summarize_chain(llm, chain_type="map_reduce")
-            summary = chain.run(st.session_state['transcript'])
+            
+            if choice == 'GeminiPro':
+                prompt = f"Summarize the following text: {st.session_state['transcript']}"
+                genai.configure(api_key = st.session_state['api_keys']['openai'])
+                # Initialize the text-based GenerativeModel with "gemini-pro"
+                model = genai.GenerativeModel("gemini-pro")
+                # Generate content based on the user's text prompt
+                response = model.generate_content(prompt)       
+                # Display the generated content as Markdown
+                summary = response.text
+                
+            elif choice == 'OpenAI':
+            
+                llm = OpenAI(openai_api_key=st.session_state['api_keys']['openai'], temperature=0.5)
+                chain = load_summarize_chain(llm, chain_type="map_reduce")
+                summary = chain.run(st.session_state['transcript'])
+            
+            
             st.subheader('Summary')
-            st.write(summary)
+            st.markdown(summary)
 
 if __name__ == "__main__":
     main()
